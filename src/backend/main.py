448a837101a6a73430e31395a2d1efff762fb609
Naufal <naufalkps@gmail.com>
2026-08-16 01:11:24 +0700

    comment uvicorn for vercel

--- src/backend/main.py
+++ src/backend/main.py
@@ -87,6 +87,6 @@ def create_app() -> FastAPI:
 
 app = create_app()
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
